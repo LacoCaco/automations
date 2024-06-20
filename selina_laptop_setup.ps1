03dab36683f3b41da38458a056a2147bfe501380
Laco Caco <batkhuu.stutz@protonmail.com>
2024-06-20 14:43:37 +0200

    Create selina_laptop_setup.ps1
    Selina Laptop apps

---
+++ selina_laptop_setup.ps1
@@ -0,0 +1,5 @@
+winget install --id=VideoLAN.VLC -e
+winget install --id=Notepad++.Notepad++ -e
+winget install --id=RavensburgerAG.tiptoiManager -e
+winget install --id=TheDocumentFoundation.LibreOffice.LTS -e
+winget install --id=Malwarebytes.Malwarebytes -e 
