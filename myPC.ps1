62793146078a64fca762cfa318378a590e0436a3
Laco Caco <batkhuu.stutz@protonmail.com>
2025-07-26 23:17:39 +0200

    Create myPC.ps1

---
+++ myPC.ps1
@@ -0,0 +1,13 @@
+#both:
+winget install -e --id ShiftCryptoAG.BitBoxApp;
+winget install -e --id LedgerHQ.LedgerLive;
+winget install -e --id Brave.Brave;
+winget install -e --id Sinew.Enpass;
+winget install -e --id Logitech.OptionsPlus;
+winget install -e --id Malwarebytes.Malwarebytes;
+winget install -e --id Microsoft.VisualStudioCode;
+winget install -e --id Notepad++.Notepad++;
+winget install -e --id ProtonTechnologies.ProtonMailBridge;
+winget install -e --id Spotify.Spotify;
+winget install -e --id VideoLAN.VLC;
+winget install -e --id WhatsApp.WhatsApp
