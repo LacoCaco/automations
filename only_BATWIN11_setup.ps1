cf0545559d411f8275b690db126b866bc65fb894
Laco Caco <batkhuu.stutz@protonmail.com>
2025-07-26 23:18:13 +0200

    Create only_BATWIN11_setup.ps1

---
+++ only_BATWIN11_setup.ps1
@@ -0,0 +1,6 @@
+#only BATWIN11:
+winget install -e --id Discord.Discord;
+winget install -e --id Logitech.GHUB;
+winget install -e --id Nvidia.GeForceExperience;
+winget install -e --id NZXT.CAM;
+winget install -e --id Valve.Steam;
