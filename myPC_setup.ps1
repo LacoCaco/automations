a31f33ecafc502d922bb8f54587d6169dbc51bda
Laco Caco <batkhuu.stutz@protonmail.com>
2025-07-28 19:19:20 +0200

    Update myPC_setup.ps1

--- myPC_setup.ps1
+++ myPC_setup.ps1
@@ -2,12 +2,12 @@
 winget install -e --id ShiftCryptoAG.BitBoxApp;
 winget install -e --id LedgerHQ.LedgerLive;
 winget install -e --id Brave.Brave;
-winget install -e --id Sinew.Enpass;
+#winget install -e --id Sinew.Enpass;  #other version available in MSSTORE
 winget install -e --id Logitech.OptionsPlus;
 winget install -e --id Malwarebytes.Malwarebytes;
 winget install -e --id Microsoft.VisualStudioCode;
 winget install -e --id Notepad++.Notepad++;
 winget install -e --id ProtonTechnologies.ProtonMailBridge;
-winget install -e --id Spotify.Spotify;
+winget install -e --id Spotify.Spotify;  #install under normal user
 winget install -e --id VideoLAN.VLC;
-winget install -e --id WhatsApp.WhatsApp
+#winget install -e --id WhatsApp.WhatsApp  #seems to be not available anymore
